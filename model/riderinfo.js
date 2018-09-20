02bf66d1de5dfdeec7785271cc83586b5a33dd4f
sumati <sumati.patel@hotmail.com>
2018-09-20 19:48:10 -0400

    rider database

--- model/riderinfo.js
+++ model/riderinfo.js
@@ -3,8 +3,12 @@ var sequelize = require("../config/connection.js");
 
 var Rider = sequelize.define("Rider", {
     name: Sequelize.STRING,
+    number: Sequelize.STRING,
+    gender: Sequelize.STRING,
     destination: Sequelize.STRING,
     route: Sequelize.STRING,
+    passengers: Sequelize.STRING,
+    rating: Sequelize.STRING,
 });
 
 Rider.sync();
--- schema.sql
+++ schema.sql
@@ -1,2 +1,24 @@
-DROP DATABASE IF EXISTS `rider`;
-CREATE DATABASE `rider`;
+riders
+DROP DATABASE IF EXISTS rider;
+CREATE DATABASE rider;
+USE rider;
+
+CREATE TABLE riders (
+  id INTEGER(11) NOT NULL,
+  FirstName VARCHAR(100),
+  LastName VARCHAR(100),
+  PhoneNumber VARCHAR (15),
+  Gender VARCHAR (10),
+  Destination VARCHAR (100),
+  Passengers INTEGER (10),
+  Rating VARCHAR (10),
+
+  PRIMARY KEY (id)
+);
+
+INSERT INTO riders (firstName, lastName, PhoneNumber, Gender, Destination, Passengers, Rating) values ('Jane', 'Austen', '416-216-6612', 'Female', 'Niagara', '2', '5/5');
+INSERT INTO riders (firstName, lastName, PhoneNumber, Gender, Destination, Passengers, Rating) values ('Mark', 'Twain', '439-772-6182', 'Male', 'Downtown Toronto', '1', '4.5/5');
+
+
+
+SELECT * FROM rider
